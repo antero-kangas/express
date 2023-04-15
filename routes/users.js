a560adb7c3f4c419671ba26c0ac0fe1efe78e5ab
Antero Kangas <antero.kangas@abako.fi>
2023-04-15 18:46:18 +0300

    [ADD] opiskeltu

---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	console.log(req.query.name);
+	res.send("User List");
+});
+
+router.get('/new', (req, res) => {
+	res.render("users/new", {firstName: "Test"});
+	// res.send("User New Form");
+});
+
+router.post('/', (req, res) => {
+	const isValid = true;
+	if (isValid) {
+		users.push({firstName: req.body.firstName});
+		res.redirect(`/users/${users.length-1}`);
+	} else {
+		console.log("Error");
+		res.render("users/new", {firstName: req.body.firstName})
+	}
+	// console.log(req.body.firstName);
+	// res.send("Create User");
+});
+
+router
+.route('/:id')
+.get((req, res) => {
+	console.log(req.user)
+	res.send(`Get User With ID ${req.params.id}`);
+})
+.put((req, res) => {
+	res.send(`Update User With ID ${req.params.id}`);
+})
+.delete((req, res) => {
+	res.send(`Delete User With ID ${req.params.id}`);
+});
+
+const users = [{name: "Kyle"}, {name: "Sally"}];
+router.param('id', (req, tes, next, id) => {
+	req.user = users[id]
+	next();
+})
+
+module.exports = router;
